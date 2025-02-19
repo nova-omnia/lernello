9d9fc3c216b1de16511b7c0391517128a1612cd5
josias-r <josias.ribi@gmail.com>
2025-02-19 13:30:14 +0100

    chore: init backend/frontend/litellm structure

---
+++ backend/src/main/java/ch/nova_omnia/pm4/Pm4Application.java
@@ -0,0 +1,13 @@
+package ch.nova_omnia.pm4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pm4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pm4Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ch/nova_omnia/pm4/Pm4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.nova_omnia.pm4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pm4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

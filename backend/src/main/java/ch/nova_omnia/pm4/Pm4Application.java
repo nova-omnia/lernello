2a795194a26cb4e34de85ae02b554055a2599564
josias-r <josias.ribi@gmail.com>
2025-02-20 10:15:44 +0100

    feat: add logging for application example

--- backend/src/main/java/ch/nova_omnia/pm4/Pm4Application.java
+++ backend/src/main/java/ch/nova_omnia/pm4/Pm4Application.java
@@ -1,12 +1,16 @@
 package ch.nova_omnia.pm4;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Pm4Application {
+	private static Logger log = LoggerFactory.getLogger(Pm4Application.class);
 
 	public static void main(String[] args) {
+		log.info("Starting PM4 application");
 		SpringApplication.run(Pm4Application.class, args);
 	}
 

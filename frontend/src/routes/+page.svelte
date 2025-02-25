b1b478cb018e2881f0fd8313cdfdaec105386a8d
josias-r <josias.ribi@gmail.com>
2025-02-25 09:52:27 +0100

    chore: init skeleton v3

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Hi its me to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Accordion } from '@skeletonlabs/skeleton-svelte';
+	// Icons
+	import Club from 'lucide-svelte/icons/club';
+	import Diamond from 'lucide-svelte/icons/diamond';
+	import Heart from 'lucide-svelte/icons/heart';
+	import Spade from 'lucide-svelte/icons/spade';
+
+	const lorem =
+		'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit esse nisi eligendi fuga! Quas nisi repellat adipisci animi repellendus incidunt laborum sunt qui nesciunt, ducimus saepe sapiente sed ut labore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit esse nisi eligendi fuga! Quas nisi repellat adipisci animi repellendus incidunt laborum sunt qui nesciunt, ducimus saepe sapiente sed ut labore.';
+
+	const value = $state(['club']);
+</script>
+
+<div class="mx-auto max-w-lg p-8">
+	<h1 class="mb-4 text-4xl font-bold">This is Skeleton!</h1>
+	<Accordion {value}>
+		<Accordion.Item value="club">
+			<!-- Control -->
+			{#snippet lead()}<Club size={24} />{/snippet}
+			{#snippet control()}Club{/snippet}
+			<!-- Panel -->
+			{#snippet panel()}{lorem}{/snippet}
+		</Accordion.Item>
+		<hr class="hr" />
+		<Accordion.Item value="diamond">
+			{#snippet lead()}<Diamond size={24} />{/snippet}
+			{#snippet control()}Diamond{/snippet}
+			{#snippet panel()}{lorem}{/snippet}
+		</Accordion.Item>
+		<hr class="hr" />
+		<Accordion.Item value="heart" disabled>
+			{#snippet lead()}<Heart size={24} />{/snippet}
+			{#snippet control()}Heart (disabled){/snippet}
+			{#snippet panel()}{lorem}{/snippet}
+		</Accordion.Item>
+		<hr class="hr" />
+		<Accordion.Item value="spade">
+			{#snippet lead()}<Spade size={24} />{/snippet}
+			{#snippet control()}Spade{/snippet}
+			{#snippet panel()}{lorem}{/snippet}
+		</Accordion.Item>
+	</Accordion>
+</div>

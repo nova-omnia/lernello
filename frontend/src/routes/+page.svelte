594e29e8302828a13a2ca9053acad600ac7d335f
josias-r <josias.ribi@gmail.com>
2025-03-13 14:31:33 +0100

    chore: format and improve styling

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
-
 </script>
 
-<main class="flex flex-col items-center justify-center h-screen">
-    <form method="POST" action="?/loginUser" class="card preset-filled-surface-100-900 border-[1px] border-surface-200-800 w-full max-w-lg p-8 space-y-8">
-        <h1 class="h2">Login</h1>
-        <div class="space-y-4">
-            <label class="label">
-                <span class="label-text">Email</span>
-                <input class="input bg-surface-50" type="text" placeholder="email"/>
-            </label>
-            <label class="label">
-                <span class="label-text">Passwort</span>
-                <input class="input bg-surface-50" type="password" placeholder="password"/>
-            </label>
-        </div>
-        <button class="btn preset-filled-primary-500 w-full">Sign in</button>
-    </form>
+<main class="flex h-full flex-col items-center justify-center">
+	<form
+		method="POST"
+		action="?/loginUser"
+		class="card preset-filled-surface-100-900 border-surface-200-800 w-full max-w-lg space-y-8 border-[1px] p-8"
+	>
+		<h1 class="h2">Login</h1>
+		<div class="space-y-4">
+			<label class="label">
+				<span class="label-text">Email</span>
+				<input class="input preset-filled-surface-200-800" type="text" placeholder="email" />
+			</label>
+			<label class="label">
+				<span class="label-text">Passwort</span>
+				<input class="input preset-filled-surface-200-800" type="password" placeholder="password" />
+			</label>
+		</div>
+		<button class="btn preset-filled-primary-500 w-full">Sign in</button>
+	</form>
 </main>

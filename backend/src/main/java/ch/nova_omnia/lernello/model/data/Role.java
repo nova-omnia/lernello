322efdfeeb9116293f7b22ca873408bf37e7abeb
verdiant <verdiant@student.zhaw.ch>
2025-03-20 11:26:22 +0100

    feat: Add role and clean up

---
+++ backend/src/main/java/ch/nova_omnia/lernello/model/data/Role.java
@@ -0,0 +1,5 @@
+package ch.nova_omnia.lernello.model.data;
+
+public enum Role {
+    INSTRUCTOR, TRAINEE
+}
--- backend/src/main/java/ch/nova_omnia/lernello/model/data/User.java
+++ backend/src/main/java/ch/nova_omnia/lernello/model/data/User.java
@@ -1,18 +1,17 @@
 package ch.nova_omnia.lernello.model.data;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 // TODO Check if user class will be used in the future
 @Getter
@@ -23,11 +22,33 @@ import lombok.Setter;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "uuid", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "e_mail", nullable = false, unique = true)
     @NotNull
-    private UUID uuid;
-    @Column(unique = true)
-    private String username;
-    private String password;
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    @NotNull
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String passwordHash;
+
+    @Column(name = "language", nullable = false)
+    @NotNull
+    private String language;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    @NotNull(message = "Role is required")
+    private Role role;
+
+    @Column(name = "create_date", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createDate;
+
+    @Column(name = "update_date")
+    private LocalDateTime updateDate;
 }

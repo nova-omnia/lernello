00b427b151181356c7c2f40c601f520fbcaadfb3
verdiant <verdiant@student.zhaw.ch>
2025-03-20 11:29:10 +0100

    feat: Add validations

--- backend/src/main/java/ch/nova_omnia/lernello/model/data/User.java
+++ backend/src/main/java/ch/nova_omnia/lernello/model/data/User.java
@@ -1,7 +1,6 @@
 package ch.nova_omnia.lernello.model.data;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
@@ -11,8 +10,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;

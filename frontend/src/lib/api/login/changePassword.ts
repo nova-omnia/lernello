70e915353d8fbb874478db7dd17452c8dcbbfc09
deomiarn <deomicarnel@hotmail.com>
2025-03-21 22:00:47 +0100

    chore: add Content-Type header to change password API request

--- frontend/src/lib/api/login/changePassword.ts
+++ frontend/src/lib/api/login/changePassword.ts
@@ -9,6 +9,9 @@ export async function changePassword(
 	changedPasswordUser: ChangedPasswordUser
 ): Promise<ChangePasswordStatus> {
 	const changePasswordRes = await userRequest(`/api/user/change-password`, 'POST', {
+		headers: {
+			'Content-Type': 'application/json'
+		},
 		body: JSON.stringify(changedPasswordUser)
 	});
 	return ChangePasswordStatusSchema.parse(changePasswordRes);

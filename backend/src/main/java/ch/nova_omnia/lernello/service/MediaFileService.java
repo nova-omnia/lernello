2d573befb6ecf5ebcfbaba3e2bd9518059a515c7
Michal Johnson <johnsmic@students.zhaw.ch>
2025-03-24 10:02:34 +0100

    fix: removed unnecessary path

--- backend/src/main/java/ch/nova_omnia/lernello/service/MediaFileService.java
+++ backend/src/main/java/ch/nova_omnia/lernello/service/MediaFileService.java
@@ -1,26 +1,19 @@
 package ch.nova_omnia.lernello.service;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import ch.nova_omnia.lernello.model.data.MediaFile;
 import ch.nova_omnia.lernello.repository.MediaFileRepository;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class MediaFileService {
     private final MediaFileRepository fileRepository;
-    private final Path fileStorageLocation;
-
-    public MediaFileService(MediaFileRepository fileRepository) {
-        this.fileRepository = fileRepository;
-        this.fileStorageLocation = Paths.get("file_storage").toAbsolutePath().normalize();
-    }
 
     public List<MediaFile> findAll() {
         return fileRepository.findAll();
@@ -36,15 +29,5 @@ public class MediaFileService {
 
     public MediaFile save(MediaFile file) {
         return fileRepository.save(file);
-    }
-
-    public void storeFile(MultipartFile file) {
-        String fileName = file.getOriginalFilename();
-        try {
-            Path targetLocation = this.fileStorageLocation.resolve(fileName);
-            file.transferTo(targetLocation);
-        } catch (Exception ex) {
-            throw new RuntimeException("Could not store file ", ex);
-        }
     }
 }

152043164887163b39c4e0374a6af8e881cffa05
verdiant <verdiant@student.zhaw.ch>
2025-03-24 10:40:24 +0100

    feat: Clean up and improve api

--- backend/src/main/java/ch/nova_omnia/lernello/api/CreateLearningKitController.java
+++ backend/src/main/java/ch/nova_omnia/lernello/api/CreateLearningKitController.java
@@ -1,11 +1,12 @@
 package ch.nova_omnia.lernello.api;
 
-import ch.nova_omnia.lernello.dto.request.CreateLerningKitDTO;
+import ch.nova_omnia.lernello.dto.request.CreateLearningKitDTO;
 import ch.nova_omnia.lernello.dto.response.LearningKitResDTO;
 import ch.nova_omnia.lernello.mapper.LearningKitMapper;
-import ch.nova_omnia.lernello.repository.LearningKitRepository;
+import ch.nova_omnia.lernello.service.LearningKitService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,18 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/learning-kits") //TODO: what does this do or where do i have to implement this??
 @Validated
 public class CreateLearningKitController {
-    private final LearningKitRepository repository;
+    private final LearningKitService learningKitService;
+
     private final LearningKitMapper learningKitMapper;
 
-    public CreateLearningKitController(LearningKitRepository repository, LearningKitMapper learningKitMapper) {
-        this.repository = repository;
+    public CreateLearningKitController(LearningKitService learningKitService, LearningKitMapper learningKitMapper) {
+        this.learningKitService = learningKitService;
         this.learningKitMapper = learningKitMapper;
     }
 
     @PostMapping("/learning-kits")
-    public @Valid ResponseEntity<LearningKitResDTO> create(@Valid @RequestBody CreateLerningKitDTO learningKit) {
+    @PreAuthorize("hasAuthority('SCOPE_kits:write')")
+    public @Valid ResponseEntity<LearningKitResDTO> create(@Valid @RequestBody CreateLearningKitDTO learningKit) {
         var entity = learningKitMapper.toEntity(learningKit);
-        var savedEntity = repository.save(entity);
+        var savedEntity = learningKitService.save(entity);
         return ResponseEntity.ok(learningKitMapper.toDTO(savedEntity));
     }
 }

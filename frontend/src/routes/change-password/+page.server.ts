073cc2fecd78f69f03a33425b88e370b9bf1b729
josias-r <josias.ribi@gmail.com>
2025-03-25 16:12:10 +0100

    feat: improve change password error handling and add redirect logic

--- frontend/src/routes/change-password/+page.server.ts
+++ frontend/src/routes/change-password/+page.server.ts
@@ -1,7 +1,7 @@
 import type { Actions } from '@sveltejs/kit';
 import { fail, redirect } from '@sveltejs/kit';
-import { handleApiError } from '$lib/api/apiError';
-import { superValidate } from 'sveltekit-superforms';
+import { ApiError, handleApiError } from '$lib/api/apiError';
+import { setError, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { ChangePasswordDataSchema } from '$lib/models/changePasswordData';
 import { changePassword } from '$lib/api/login/changePassword';
@@ -19,13 +19,20 @@ export const actions = {
 		if (!form.valid) {
 			return fail(400, { form });
 		}
-		const changePasswordStatus = await changePassword(form.data);
-		const url = new URL(request.url);
-		const redirectTo = url.searchParams.get('redirectTo') || '/dashboard';
-		if (changePasswordStatus.success) {
-			return redirect(303, redirectTo);
+		try {
+			const changePasswordStatus = await changePassword(form.data);
+			const url = new URL(request.url);
+			const redirectTo = url.searchParams.get('redirectTo') || '/dashboard';
+			if (changePasswordStatus.success) {
+				return redirect(303, redirectTo);
+			}
+		} catch (error) {
+			if (error instanceof ApiError) {
+				if (error.status === 403) {
+					return setError(form, 'newPassword', 'Not allowed to change password');
+				}
+			}
+			throw error;
 		}
-
-		return fail(400, { form });
 	})
 } satisfies Actions;

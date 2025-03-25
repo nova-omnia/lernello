567f6d247158d214279ba35c44bb20bb58a5d373
deomiarn <deomicarnel@hotmail.com>
2025-03-25 23:27:04 +0100

    feat: error messages showing in FE

--- frontend/src/routes/change-password/+page.server.ts
+++ frontend/src/routes/change-password/+page.server.ts
@@ -1,38 +1,64 @@
 import type { Actions } from '@sveltejs/kit';
 import { fail, redirect } from '@sveltejs/kit';
 import { ApiError, handleApiError } from '$lib/api/apiError';
-import { setError, superValidate } from 'sveltekit-superforms';
+import { setError, superValidate, type SuperValidated } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { ChangePasswordDataSchema } from '$lib/models/changePasswordData';
 import { changePassword } from '$lib/api/login/changePassword';
 import { requireLogin } from '$lib/server/auth';
 
-export const load = async () => {
+export const load = async ({ request }) => {
+	const user = requireLogin();
+
+	//TODO changedPassword not updated
+	if (user.changedPassword) {
+		const redirectTo = new URL(request.url).searchParams.get('redirectTo') || '/dashboard';
+		return redirect(303, redirectTo);
+	}
+
 	const form = await superValidate(zod(ChangePasswordDataSchema));
 	return { form };
 };
 
+function handleApiFormError(
+	form: SuperValidated<any>,
+	error: ApiError,
+	field: string = 'confirmPassword'
+) {
+	switch (error.status) {
+		case 400:
+			return setError(form, field, 'Invalid password');
+		case 401:
+			return setError(form, field, 'Unauthorized: Please log in');
+		case 403:
+			return setError(form, field, 'Not allowed, permission denied');
+		default:
+			return setError(form, field, error.message || 'An unknown error occurred');
+	}
+}
+
 export const actions = {
 	changePassword: handleApiError(async ({ request }) => {
 		requireLogin();
+
 		const form = await superValidate(request, zod(ChangePasswordDataSchema));
 		if (!form.valid) {
 			return fail(400, { form });
 		}
+
 		try {
-			const changePasswordStatus = await changePassword(form.data);
-			const url = new URL(request.url);
-			const redirectTo = url.searchParams.get('redirectTo') || '/dashboard';
-			if (changePasswordStatus.success) {
-				return redirect(303, redirectTo);
+			const { success } = await changePassword(form.data);
+			if (!success) {
+				return setError(form, 'confirmPassword', 'Change password failed');
 			}
 		} catch (error) {
 			if (error instanceof ApiError) {
-				if (error.status === 403) {
-					return setError(form, 'newPassword', 'Not allowed to change password');
-				}
+				return handleApiFormError(form, error);
 			}
 			throw error;
 		}
+
+		const redirectTo = new URL(request.url).searchParams.get('redirectTo') || '/dashboard';
+		return redirect(303, redirectTo);
 	})
 } satisfies Actions;
--- frontend/src/routes/login/+page.server.ts
+++ frontend/src/routes/login/+page.server.ts
@@ -2,8 +2,8 @@ import { UserLoginSchema } from '$lib/models/user';
 import { login } from '$lib/api/login/auth';
 import type { Actions } from '@sveltejs/kit';
 import { fail, redirect } from '@sveltejs/kit';
-import { handleApiError } from '$lib/api/apiError';
-import { superValidate } from 'sveltekit-superforms';
+import { ApiError, handleApiError } from '$lib/api/apiError';
+import { setError, superValidate, type SuperValidated } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 
 export const load = async () => {
@@ -11,25 +11,48 @@ export const load = async () => {
 	return { form };
 };
 
+function handleApiFormError(
+	form: SuperValidated<any>,
+	error: ApiError,
+	field: string = 'username'
+) {
+	switch (error.status) {
+		case 400:
+			return setError(form, field, 'Invalid username or password');
+		case 401:
+			return setError(form, field, 'Unauthorized: Please log in');
+		case 403:
+			return setError(form, field, 'Not allowed, permission denied');
+		default:
+			return setError(form, field, error.message || 'An unknown error occurred');
+	}
+}
+
 export const actions = {
 	login: handleApiError(async ({ request, cookies }) => {
 		const form = await superValidate(request, zod(UserLoginSchema));
 		if (!form.valid) {
 			return fail(400, { form });
 		}
+		try {
+			const loggedInUser = await login(form.data);
+			cookies.set('sessionToken', JSON.stringify(loggedInUser), {
+				httpOnly: true,
+				path: '/',
+				maxAge: loggedInUser.expires / 1000 // convert milliseconds to seconds
+			});
+			const url = new URL(request.url);
+			const redirectTo = url.searchParams.get('redirectTo') || '/dashboard';
 
-		const loggedInUser = await login(form.data);
-		cookies.set('sessionToken', JSON.stringify(loggedInUser), {
-			httpOnly: true,
-			path: '/',
-			maxAge: loggedInUser.expires / 1000 // convert milliseconds to seconds
-		});
-		const url = new URL(request.url);
-		const redirectTo = url.searchParams.get('redirectTo') || '/dashboard';
-
-		if (!loggedInUser.changedPassword) {
-			redirect(303, `/change-password?redirectTo=${encodeURIComponent(redirectTo)}`);
+			if (!loggedInUser.changedPassword) {
+				redirect(303, `/change-password?redirectTo=${encodeURIComponent(redirectTo)}`);
+			}
+			redirect(303, redirectTo);
+		} catch (error) {
+			if (error instanceof ApiError) {
+				return handleApiFormError(form, error, 'password');
+			}
+			throw error;
 		}
-		redirect(303, redirectTo);
 	})
 } satisfies Actions;

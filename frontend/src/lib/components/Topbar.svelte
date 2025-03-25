55c9a8b6216a11b165947c77837c21525c0ed1f9
Rajadurai Thunusan (rajadthu) <rajadthu@students.zhaw.ch>
2025-03-25 16:00:17 +0100

    refactor(Sidebar/Topbar): simplify sidebar toggle logic and replace icons

--- frontend/src/lib/components/Topbar.svelte
+++ frontend/src/lib/components/Topbar.svelte
@@ -1,23 +1,18 @@
 <script lang="ts">
 	import { sidebarState } from '$lib/components/sidebarState.svelte';
-	import { Sidebar } from 'lucide-svelte';
+	import { SidebarOpen } from 'lucide-svelte';
 </script>
 
-<nav class="preset-filled-surface-100-900 flex pt-1 pl-2">
-	<button
-		class="flex h-12 w-12 items-center justify-center rounded duration-100 ease-linear"
-		onclick={sidebarState.toggleSidebar}
-		aria-label="Toggle sidebar"
-		aria-expanded={!sidebarState.isExpanded}
-		class:visible={!sidebarState.isExpanded}
-		class:w-max={!sidebarState.isExpanded}
-		class:opacity-100={!sidebarState.isExpanded}
-		class:invisible={!sidebarState.isExpanded}
-		class:w-0={!sidebarState.isExpanded}
-		class:opacity-0={sidebarState.isExpanded}
-	>
-		<Sidebar size={24} />
-	</button>
+<nav class="preset-filled-surface-100-900 flex min-h-13 pt-1 pl-2">
+	{#if !sidebarState.isExpanded}
+		<button
+			class="flex h-12 w-12 items-center justify-center"
+			onclick={sidebarState.toggleSidebar}
+			aria-label="Toggle sidebar"
+		>
+			<SidebarOpen size={24} />
+		</button>
+	{/if}
 	<div
 		class="input-group disabled mr-2 ml-auto grid-cols-[auto_1fr_auto] items-center justify-center"
 	>

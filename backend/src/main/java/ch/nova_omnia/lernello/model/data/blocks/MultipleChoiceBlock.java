46b57988e2cd9bcd94f635676c0180521a9f8e3f
Michal Johnson <johnsmic@students.zhaw.ch>
2025-03-26 22:10:00 +0100

    feat: MultipleChoiceBlock and QuestionBlock entities + updated package.json

--- backend/src/main/java/ch/nova_omnia/lernello/model/data/blocks/MultipleChoiceBlock.java
+++ backend/src/main/java/ch/nova_omnia/lernello/model/data/blocks/MultipleChoiceBlock.java
@@ -1,8 +1,15 @@
 package ch.nova_omnia.lernello.model.data.blocks;
 
+import java.util.List;
+
 import ch.nova_omnia.lernello.model.data.LearningUnit;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.validation.constraints.NotNull;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -12,5 +19,42 @@ public class MultipleChoiceBlock extends Block {
 
     public MultipleChoiceBlock(String name, int position, LearningUnit learningUnit) {
         super(name, position, learningUnit);
+    }
+    @NotNull
+    @Column(name = "question", nullable = false)
+    private String question;
+
+    @ElementCollection
+    @CollectionTable(name = "multiple_choice_answers", joinColumns = @JoinColumn(name = "block_id"))
+    @Column(name = "answer")
+    private List<String> possibleAnswers;
+
+    @ElementCollection
+    @CollectionTable(name = "multiple_choice_correct_answers", joinColumns = @JoinColumn(name = "block_id"))
+    @Column(name = "correct_answer")
+    private List<String> correctAnswers;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public List<String> getPossibleAnswers() {
+        return possibleAnswers;
+    }
+
+    public void setPossibleAnswers(List<String> possibleAnswers) {
+        this.possibleAnswers = possibleAnswers;
+    }
+
+    public List<String> getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public void setCorrectAnswers(List<String> correctAnswers) {
+        this.correctAnswers = correctAnswers;
     }
 }
--- backend/src/main/java/ch/nova_omnia/lernello/model/data/blocks/QuestionBlock.java
+++ backend/src/main/java/ch/nova_omnia/lernello/model/data/blocks/QuestionBlock.java
@@ -1,8 +1,10 @@
 package ch.nova_omnia.lernello.model.data.blocks;
 
 import ch.nova_omnia.lernello.model.data.LearningUnit;
+import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -12,5 +14,29 @@ public class QuestionBlock extends Block {
     
     public QuestionBlock(String name, int position, LearningUnit learningUnit) {
         super(name, position, learningUnit);
+    }
+    
+    @NotNull
+    @Column(name = "question", nullable = false)
+    private String question;
+
+    @NotNull
+    @Column(name = "expected_answer", nullable = false)
+    private String expectedAnswer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getExpectedAnswer() {
+        return expectedAnswer;
+    }
+
+    public void setExpectedAnswer(String expectedAnswer) {
+        this.expectedAnswer = expectedAnswer;
     }
 }

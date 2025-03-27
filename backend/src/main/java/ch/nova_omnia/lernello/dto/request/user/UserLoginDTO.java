ced47faa95a36313c5f11ff3c49ef3446e782012
josias-r <josias.ribi@gmail.com>
2025-03-27 20:14:38 +0100

    feat: update UserLoginDTO to enforce password length validation

--- backend/src/main/java/ch/nova_omnia/lernello/dto/request/user/UserLoginDTO.java
+++ backend/src/main/java/ch/nova_omnia/lernello/dto/request/user/UserLoginDTO.java
@@ -1,12 +1,11 @@
 package ch.nova_omnia.lernello.dto.request.user;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UserLoginDTO(
-        @NotNull @Size(min = 3, max = 40) String username,
-        @NotBlank @Min(value = 8) String password
+                           @NotNull @Size(min = 3, max = 40) String username,
+                           @NotBlank @Size(min = 8) String password
 ) {
 }

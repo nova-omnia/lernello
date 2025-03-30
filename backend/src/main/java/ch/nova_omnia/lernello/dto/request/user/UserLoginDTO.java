bd487cbe6980bb5c8d525b4810cb111492eb35fe
deomiarn <deomicarnel@hotmail.com>
2025-03-30 08:33:41 +0200

    chore: enforce minimum password length validation in UserLoginDTO and NotBlank for username

--- backend/src/main/java/ch/nova_omnia/lernello/dto/request/user/UserLoginDTO.java
+++ backend/src/main/java/ch/nova_omnia/lernello/dto/request/user/UserLoginDTO.java
@@ -1,11 +1,11 @@
 package ch.nova_omnia.lernello.dto.request.user;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UserLoginDTO(
-                           @NotNull @Size(min = 3, max = 40) String username,
-                           @NotBlank @Size(min = 8) String password
+        @NotBlank @Size(min = 3, max = 40) String username,
+        @NotBlank @Min(8) String password
 ) {
 }

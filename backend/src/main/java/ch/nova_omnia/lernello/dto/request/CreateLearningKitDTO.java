14b01564c5df3142b3a20a5f163fb80423d08881
Julian Maag <maagjul1@students.zhaw.ch>
2025-03-31 11:28:43 +0200

    fix: error from mergeconflict and typo correction

--- backend/src/main/java/ch/nova_omnia/lernello/dto/request/CreateLearningKitDTO.java
+++ backend/src/main/java/ch/nova_omnia/lernello/dto/request/CreateLearningKitDTO.java
@@ -1,19 +1,19 @@
 package ch.nova_omnia.lernello.dto.request;
 
-import ch.nova_omnia.lernello.dto.response.UserDTO;
+import ch.nova_omnia.lernello.dto.response.user.LoggedInUserDTO;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.Date;
 import java.util.List;
 
-public record CreateLerningKitDTO(
+public record CreateLearningKitDTO(
         @NotNull @Size(min = 3, max = 40) String name,
         String description,
         @NotNull String language,
         Date dealineDate,
         Date startDate,
         Date endDate,
-        List<UserDTO> participants
+        List<LoggedInUserDTO> participants
     ){
 }

f52b88428fcf17bcee70608ed531cc1b2315bfcc
Michal Johnson <johnsmic@students.zhaw.ch>
2025-04-01 20:36:19 +0200

    feat: add file upload component to learning kit page

--- frontend/src/routes/(app)/learningkit/+page.svelte
+++ frontend/src/routes/(app)/learningkit/+page.svelte
@@ -1,5 +1,12 @@
+<script lang="ts">
+	import FileUpdate from '$lib/components/FileUpdate.svelte';
+</script>
+
 <div class="ml-4">
 	<h1 class="font-bold">LK1</h1>
 	<p>Description</p>
-	<p></p>
+	<div class="mt-5">
+		<h2 class="mb-2 text-lg font-semibold">Upload Files</h2>
+		<FileUpdate />
+	</div>
 </div>

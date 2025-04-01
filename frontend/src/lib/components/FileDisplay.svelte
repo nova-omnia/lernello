232d025fa6da8dec135a8e3db452da9e68bc3452
Julian Maag <maagjul1@students.zhaw.ch>
2025-04-01 16:20:25 +0200

    fix: prettier

--- frontend/src/lib/components/FileDisplay.svelte
+++ frontend/src/lib/components/FileDisplay.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-    import { File,Download  } from 'lucide-svelte';
-    //TODO: add dynamic name for file
+	import { File, Download } from 'lucide-svelte';
+	//TODO: add dynamic name for file
 </script>
 
-<div class="File-Display flex items-center rounded-lg border-2 rounded-border-gray-200 p-3 text-base">
-    <File class="w-10 h-10"/>
-    <p class="ml-2 text-base text-bold text-black-700">File Name</p>
-    <button type="button" class="btn preset-outlined-surface-500 bg-grey rounded-xl ml-auto p-2"><Download /></button>
-    <button type="button" class="btn preset-filled-error-500 rounded-xl ml-1 p-2">Remove</button>
+<div
+	class="File-Display rounded-border-gray-200 flex items-center rounded-lg border-2 p-3 text-base"
+>
+	<File class="h-10 w-10" />
+	<p class="text-bold text-black-700 ml-2 text-base">File Name</p>
+	<button type="button" class="btn preset-outlined-surface-500 bg-grey ml-auto rounded-xl p-2"
+		><Download /></button
+	>
+	<button type="button" class="btn preset-filled-error-500 ml-1 rounded-xl p-2">Remove</button>
 </div>

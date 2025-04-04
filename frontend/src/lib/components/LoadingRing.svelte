fb27cbfc033bb5add127cb1ba5cb4b590910ec4e
Hajir <hajir3@gmail.com>
2025-04-04 12:16:18 +0200

    fix: fixed bug where, the language would show first the fallback language before showing selected language

--- frontend/src/lib/components/LoadingRing.svelte
+++ frontend/src/lib/components/LoadingRing.svelte
@@ -2,9 +2,12 @@
 	import { ProgressRing } from '@skeletonlabs/skeleton-svelte';
 </script>
 
-<ProgressRing
-	value={null}
-	size="size-10"
-	meterStroke="stroke-tertiary-600-400"
-	trackStroke="stroke-tertiary-50-950"
-/>
+<div class="flex items-center justify-center min-h-screen">
+	<ProgressRing
+		value={null}
+		size="size-10"
+		meterStroke="stroke-tertiary-600-400"
+		trackStroke="stroke-tertiary-50-950"
+	/>
+</div>
+
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
-	import '../app.css';
-	import { ToastProvider } from '@skeletonlabs/skeleton-svelte';
-	import '$lib/i18n/i18n.ts';
-	import { isLoading } from 'svelte-i18n';
-	import LoadingRing from '$lib/components/LoadingRing.svelte';
+    import '../app.css';
+    import { ToastProvider } from '@skeletonlabs/skeleton-svelte';
+    import { i18nReady } from '$lib/i18n/i18n';
+    import LoadingRing from '$lib/components/LoadingRing.svelte';
 
-	let { children } = $props();
+    let ready = false;
+
+    i18nReady.then(() => {
+        ready = true;
+    });
 </script>
 
-{#if $isLoading}
-	<LoadingRing />
+{#if !ready}
+    <LoadingRing />
 {:else}
-	<ToastProvider>
-		{@render children()}
-	</ToastProvider>
+    <ToastProvider>
+        <slot />
+    </ToastProvider>
 {/if}

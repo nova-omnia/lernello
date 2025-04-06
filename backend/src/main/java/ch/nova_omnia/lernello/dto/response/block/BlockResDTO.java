28152291343d7f54f6429e04f76e7f5fdf8338b7
Léon Brodbeck <brodbeckleon@gmail.com>
2025-04-06 23:39:12 +0200

    feat: remove unused imports

--- backend/src/main/java/ch/nova_omnia/lernello/dto/response/block/BlockResDTO.java
+++ backend/src/main/java/ch/nova_omnia/lernello/dto/response/block/BlockResDTO.java
@@ -1,13 +1,4 @@
 package ch.nova_omnia.lernello.dto.response.block;
 
-import java.util.UUID;
-
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
-
 public sealed interface BlockResDTO permits MultipleChoiceBlockResDTO, QuestionBlockResDTO, TheoryBlockResDTO {
 }

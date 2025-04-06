2d2b8feb3438fd136a313a6fce0952907ec10d8d
Léon Brodbeck <brodbeckleon@gmail.com>
2025-04-06 15:36:10 +0200

    feat: bug fix

--- backend/src/main/java/ch/nova_omnia/lernello/mapper/TemporaryKeyMapper.java
+++ backend/src/main/java/ch/nova_omnia/lernello/mapper/TemporaryKeyMapper.java
@@ -2,11 +2,13 @@ package ch.nova_omnia.lernello.mapper;
 
 import ch.nova_omnia.lernello.dto.response.TemporaryKeySolveResDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.Map;
 import java.util.UUID;
 
 @Mapper(componentModel = "spring")
 public interface TemporaryKeyMapper {
+    @Mapping(target = "temporaryKeyMap", source = ".")
     TemporaryKeySolveResDTO toDTO(Map<String, UUID> temporaryKeyMap);
 }

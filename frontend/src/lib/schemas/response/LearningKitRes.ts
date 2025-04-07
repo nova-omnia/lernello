3cb7bed8caa29939ed59c0e9abf57dddb1adf56b
Léon Brodbeck <brodbeckleon@gmail.com>
2025-04-07 11:16:10 +0200

    feat: reset branch to 0b885ac0

--- frontend/src/lib/schemas/response/LearningKitRes.ts
+++ frontend/src/lib/schemas/response/LearningKitRes.ts
@@ -4,13 +4,13 @@ export const LearningKitResSchema = z.object({
 	uuid: z.string().uuid().nonempty(),
 	name: z.string().nonempty(),
 	learningUnits: z.array(z.any()).optional(), //ToDo
-	description: z.string().optional(),
+	description: z.string().optional().nullable(),
 	deadlineDate: z.preprocess(
 		(val) => (typeof val === 'string' ? new Date(val) : undefined),
 		z.date().optional()
 	),
-	language: z.string().nonempty(),
+	language: z.string().optional().nullable(),
 	participants: z.array(z.string().uuid()),
-	folderId: z.string().uuid().nullable()
+	folderId: z.string().uuid().optional().nullable()
 });
 export type LearningKitRes = z.infer<typeof LearningKitResSchema>;

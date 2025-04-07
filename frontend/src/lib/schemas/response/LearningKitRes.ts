e18873ee3a3a3ef653463752dfe2eda59b3f7ce4
Léon Brodbeck <brodbeckleon@gmail.com>
2025-04-07 11:03:34 +0200

    feat: made folderId, description and language nullable

--- frontend/src/lib/schemas/response/LearningKitRes.ts
+++ frontend/src/lib/schemas/response/LearningKitRes.ts
@@ -4,13 +4,13 @@ export const LearningKitResSchema = z.object({
 	uuid: z.string().uuid().nonempty(),
 	name: z.string().nonempty(),
 	learningUnits: z.array(z.any()).optional(), //ToDo
-	description: z.string().optional(),
+	description: z.string().optional().nullable(),
 	deadlineDate: z.preprocess(
 		(val) => (typeof val === 'string' ? new Date(val) : undefined),
 		z.date().optional()
 	),
-	language: z.string().nonempty().optional(), //TODO
+	language: z.string().optional().nullable(),
 	participants: z.array(z.string().uuid()),
-	folderId: z.string().uuid().optional().nullable() //TODO
+	folderId: z.string().uuid().optional().nullable()
 });
 export type LearningKitRes = z.infer<typeof LearningKitResSchema>;

ba5cf9fe1875c0c40506491229ab10a37b11873a
Julian Maag <maagjul1@students.zhaw.ch>
2025-04-08 10:31:38 +0200

    fix: styling

--- frontend/src/routes/(app)/learningkit/[learningKitId]/+page.svelte
+++ frontend/src/routes/(app)/learningkit/[learningKitId]/+page.svelte
@@ -8,6 +8,8 @@
 	let { data } = $props();
 	const kit = data.kit;
 
+	const learningUnits = kit.learningUnits || [];
+
 	function formatDate(date: Date): string {
 		const day = String(date.getDate()).padStart(2, '0');
 		const month = String(date.getMonth() + 1).padStart(2, '0'); // months are 0-based
@@ -20,7 +22,7 @@
 	};
 </script>
 
-<div class="p-4">
+<div class="p-4 bg-surface-50-950">
 	<!--header-->
 	<div class="flex space-between items-start p-1">
 		<div>
@@ -29,25 +31,27 @@
 		<p class="mt-2 flex items-center"><Clock class="inline-block mr-2" /> {formatDate(kit.deadlineDate)}</p>
 		</div>
 
-		<button class="btn preset-outlined-surface-500 ml-auto rounded-full p-2"><Settings/>Edit</button>
+		<button type="button" class="btn preset-outlined-surface-500 ml-auto rounded-full p-2"><Settings/>Edit</button>
 	</div>
 
 	<!--content-->
 	<p class="mt-5 text-sm text-primary-500 font-semibold">Content</p>
 	<p class="mt-5 text-sm">The contents of the LearningKit</p>
 	<!-- foreach learningUnit-->
-	<LearningUnitDisplay {learningUnit}/>
-	<CheckpointDisplay />
-	<button class="btn preset-outlined-surface-500 ml-auto w-full rounded-xl p-2" ><Plus></Plus>Create new Learning Kit</button>
+	{#each learningUnits as learningUnit}
+		<LearningUnitDisplay {learningUnit}/>
+	{/each}
+	<CheckpointDisplay/>
+	<button type="button" class="btn preset-outlined-surface-500 ml-auto w-full rounded-xl p-2" ><Plus></Plus>Create new Learning Kit</button>
 
 	<!-- trainee -->
 	<p class="mt-5 text-sm text-primary-500 font-semibold">Trainees</p>
 	<p class="mt-5 text-sm">These Trainees have access to the course</p>
 	<!-- foreach trainee-->
 	<div class="flex flex-col gap-2">
-	<TraineeDisplay User={{name: 'Tim'}}/>
-	<TraineeDisplay User={{name: 'Tim'}}/>
-	<TraineeDisplay User={{name: 'Tim'}}/>
+		<TraineeDisplay User={{name: 'Tim'}}/>
+
+		<button type="button" class="btn preset-outlined-surface-500 ml-auto w-full rounded-xl p-2" ><Plus></Plus>Add Trainee</button>
 	</div>
 
 	<!-- Context -->
@@ -55,9 +59,9 @@
 	<p class="mt-5 text-sm">The context provided to the AI assisting tools</p>
 	<!-- foreach context-->
 	<div class="flex flex-col gap-2">
-	<FileDisplay />
-	<FileDisplay />
-	<FileUpload />
+		<FileDisplay />
+		<FileDisplay />
+		<FileUpload />
 	</div>
 
 </div>

468eb84a9ea90bae053c98bf0c3243797d83851d
josias-r <josias.ribi@gmail.com>
2025-04-08 16:26:04 +0200

    fix: remove redundant register call for English locale

--- frontend/src/lib/i18n/i18n.ts
+++ frontend/src/lib/i18n/i18n.ts
@@ -1,7 +1,6 @@
 import { init, register } from 'svelte-i18n';
 
 register('en', () => import('./locales/en.json'));
-register('en', () => import('./locales/en.json'));
 register('de', () => import('./locales/de.json'));
 register('fr', () => import('./locales/fr.json'));
 register('it', () => import('./locales/it.json'));

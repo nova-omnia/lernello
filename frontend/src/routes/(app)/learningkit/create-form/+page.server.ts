fba2bf89ef828ecdbdc5564544c655e62b694d61
verdiant <verdiant@student.zhaw.ch>
2025-04-10 23:19:54 +0200

    fix: prettier

--- frontend/src/routes/(app)/learningkit/create-form/+page.server.ts
+++ frontend/src/routes/(app)/learningkit/create-form/+page.server.ts
@@ -1,38 +1,36 @@
-import {superValidate} from 'sveltekit-superforms';
-import {zod} from 'sveltekit-superforms/adapters';
-import {handleApiError} from '$lib/api/apiError';
-import {type Actions, fail, redirect} from '@sveltejs/kit';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { handleApiError } from '$lib/api/apiError';
+import { type Actions, fail, redirect } from '@sveltejs/kit';
 import {
-    CreateLearningKitSchema,
-    EditLearningKitSchema
+	CreateLearningKitSchema,
+	EditLearningKitSchema
 } from '$lib/schemas/request/CreateLearningKit';
-import {serverApiClient} from '$lib/api/serverApiClient';
-import {
-    createLearningKit
-} from '$lib/api/collections/learningKit';
+import { serverApiClient } from '$lib/api/serverApiClient';
+import { createLearningKit } from '$lib/api/collections/learningKit';
 
 let editId: string | null;
 export const load = async () => {
-    let formData;
+	let formData;
 
-    const form = await superValidate(formData, zod(EditLearningKitSchema));
-    return {form};
+	const form = await superValidate(formData, zod(EditLearningKitSchema));
+	return { form };
 };
 
 export const actions = {
-    create: handleApiError(async ({request}) => {
-        const form = await superValidate(request, zod(CreateLearningKitSchema));
-        if (!form.valid) {
-            return fail(400, {form});
-        }
+	create: handleApiError(async ({ request }) => {
+		const form = await superValidate(request, zod(CreateLearningKitSchema));
+		if (!form.valid) {
+			return fail(400, { form });
+		}
 
-        let learningKit = await serverApiClient.req(createLearningKit, {
-            ...form.data,
-            deadlineDate: form.data.deadlineDate
-                ? form.data.deadlineDate.toISOString().split('T')[0]
-                : null
-        });
+		let learningKit = await serverApiClient.req(createLearningKit, {
+			...form.data,
+			deadlineDate: form.data.deadlineDate
+				? form.data.deadlineDate.toISOString().split('T')[0]
+				: null
+		});
 
-        return redirect(303, `/learningkit/${learningKit.uuid}`);
-    })
+		return redirect(303, `/learningkit/${learningKit.uuid}`);
+	})
 } satisfies Actions;

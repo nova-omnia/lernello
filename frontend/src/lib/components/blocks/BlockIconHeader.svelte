c18273a57d44c38c939862fa9667005e49a30584
Léon Brodbeck <brodbeckleon@gmail.com>
2025-04-13 14:46:52 +0200

    feat: implemented language to learning unit

--- frontend/src/lib/components/blocks/BlockIconHeader.svelte
+++ frontend/src/lib/components/blocks/BlockIconHeader.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { _ } from 'svelte-i18n';
 	import type { BlockRes } from '$lib/schemas/response/BlockRes';
 	import BlockIcon from './BlockIcon.svelte';
 
@@ -6,10 +7,23 @@
 		block: BlockRes;
 	}
 	const { block }: BlockIconHeaderProps = $props();
+
+	let blockType: string = $state(block.type);
+
+	switch (block.type) {
+		case 'THEORY':
+			blockType = 'block.theoryBlock';
+			break;
+		case 'MULTIPLE_CHOICE':
+			blockType = 'block.multipleChoiceQuiz';
+			break;
+		default:
+			blockType = 'Unknown Block';
+	}
 </script>
 
 <div class="flex items-end gap-2">
 	<BlockIcon iconType={block.type} />
 	<h3 class="font-medium">{block.name}</h3>
-	<span class="text-sm text-gray-500">({block.type})</span>
+	<span class="text-sm text-gray-500">({$_(blockType)})</span>
 </div>

52829bcba54a1c22c7c62fbff480dc2d6613d53d
Léon Brodbeck <brodbeckleon@gmail.com>
2025-04-13 15:32:13 +0200

    feat: pr comment

--- frontend/src/lib/components/blocks/BlockIconHeader.svelte
+++ frontend/src/lib/components/blocks/BlockIconHeader.svelte
@@ -8,22 +8,22 @@
 	}
 	const { block }: BlockIconHeaderProps = $props();
 
-	let blockType: string = $derived(block.type);
-
-	switch (block.type) {
-		case 'THEORY':
-			blockType = 'block.theoryBlock';
-			break;
-		case 'MULTIPLE_CHOICE':
-			blockType = 'block.multipleChoiceQuiz';
-			break;
-		default:
-			blockType = 'Unknown Block';
-	}
+	let blockTypeTerm = $derived.by(() => {
+		switch (block.type) {
+			case 'THEORY':
+				return 'block.theoryBlock';
+				break;
+			case 'MULTIPLE_CHOICE':
+				return 'block.multipleChoiceQuiz';
+				break;
+			default:
+				return 'Unknown Block';
+		}
+	});
 </script>
 
 <div class="flex items-end gap-2">
 	<BlockIcon iconType={block.type} />
 	<h3 class="font-medium">{block.name}</h3>
-	<span class="text-sm text-gray-500">({$_(blockType)})</span>
+	<span class="text-sm text-gray-500">({$_(blockTypeTerm)})</span>
 </div>

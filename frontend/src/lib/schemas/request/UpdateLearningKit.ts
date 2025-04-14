82586335451fdfe47e437df2aef29f7936d0543b
verdiant <verdiant@student.zhaw.ch>
2025-04-14 14:43:25 +0200

    fix: lint

--- frontend/src/lib/schemas/request/UpdateLearningKit.ts
+++ frontend/src/lib/schemas/request/UpdateLearningKit.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { CreateLearningKitSchema } from '$lib/schemas/request/CreateLearningKit';
 
 export const UpdateLearningKitSchema = CreateLearningKitSchema.extend({
+	uuid: z.string().uuid().nonempty(),
 	participants: z.array(z.string().uuid()).nullable(),
 	files: z.array(z.string().uuid()).nullable()
 });

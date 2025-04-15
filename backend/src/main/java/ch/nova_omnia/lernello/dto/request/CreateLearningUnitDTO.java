627784007c02555f06a003d3071199388673f8db
Léon Brodbeck <brodbeckleon@gmail.com>
2025-04-15 09:17:24 +0200

    feat: update dto

--- backend/src/main/java/ch/nova_omnia/lernello/dto/request/CreateLearningUnitDTO.java
+++ backend/src/main/java/ch/nova_omnia/lernello/dto/request/CreateLearningUnitDTO.java
@@ -1,7 +1,16 @@
 package ch.nova_omnia.lernello.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record CreateLearningUnitDTO(@NotBlank @Size(min = 2, max = 32) String name) {
+import java.util.UUID;
+
+public record CreateLearningUnitDTO(
+    @NotBlank
+    @Size(min = 2, max = 32)
+    String name,
+    @NotNull
+    UUID learningKitId
+    ) {
 }

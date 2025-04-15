434775661f4220da9ada5af891a6a8fee91cbe71
josias-r <josias.ribi@gmail.com>
2025-04-15 16:25:46 +0200

    chore: fix lint issues

--- frontend/src/routes/(app)/dashboard/+page.svelte
+++ frontend/src/routes/(app)/dashboard/+page.svelte
@@ -45,7 +45,7 @@
 
 	{#if $kitsQuery.status === 'pending'}
 		<div class="mt-5 flex flex-wrap gap-5">
-			{#each Array(3) as _, idx (idx)}
+			{#each Array(3)}
 				<div class="card preset-filled-surface-100-900 grid w-52 gap-2 p-5">
 					<div class="placeholder animate-pulse"></div>
 					<div class="placeholder animate-pulse"></div>

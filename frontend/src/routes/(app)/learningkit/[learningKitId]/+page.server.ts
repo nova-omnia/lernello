aba4f4227f27913c4ccae1aa7e68362cc7fe7f28
Hajir <hajir3@gmail.com>
2025-04-17 00:26:54 +0200

    fix: include allFiles in load function to ensure complete data retrieval

--- frontend/src/routes/(app)/learningkit/[learningKitId]/+page.server.ts
+++ frontend/src/routes/(app)/learningkit/[learningKitId]/+page.server.ts
@@ -1,14 +1,17 @@
 import { getLearningKitById } from '$lib/api/collections/learningKit';
 import { handleApiError } from '$lib/api/apiError';
 import { getAllTrainees } from '$lib/api/collections/user';
+import { getAllFiles } from '$lib/api/collections/file.js';
 import { api } from '$lib/api/apiClient.js';
 
 export const load = handleApiError(async ({ params, fetch }) => {
 	const kit = await api(fetch).req(getLearningKitById, null, params.learningKitId).parse();
 	const allTrainees = await api(fetch).req(getAllTrainees, null).parse();
+	const allFiles = await api(fetch).req(getAllFiles, null).parse();
 
 	return {
 		kitToDisplay: kit,
-		allTrainees
+		allTrainees,
+		allFiles
 	};
 });

7dda8589a6887ec8c948794270d60c1888938797
josias-r <josias.ribi@gmail.com>
2025-04-17 11:56:17 +0200

    refactor: redefine UpdateLearningKitSchema to remove dependency on CreateLearningKitSchema

--- frontend/src/lib/schemas/request/UpdateLearningKit.ts
+++ frontend/src/lib/schemas/request/UpdateLearningKit.ts
@@ -1,7 +1,6 @@
 import { z } from 'zod';
-import { CreateLearningKitSchema } from '$lib/schemas/request/CreateLearningKit';
 
-export const UpdateLearningKitSchema = CreateLearningKitSchema.extend({
+export const UpdateLearningKitSchema = z.object({
 	name: z.string().min(3).max(40).optional(),
 	description: z.string().nullable().optional(),
 	deadlineDate: z.string().datetime().nullable().optional(),

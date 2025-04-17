740b938628524bc725c84e7a86d601a1e2dab9fe
Michal Johnson <johnsmic@students.zhaw.ch>
2025-04-17 20:39:39 +0200

    refactor: simplify FileUpload component and update button styling

--- frontend/src/lib/components/FileUpload.svelte
+++ frontend/src/lib/components/FileUpload.svelte
@@ -3,17 +3,9 @@
 	import { Upload as IconUpload, File as IconFile, XCircle as IconRemove } from 'lucide-svelte';
 </script>
 
-<FileUpload
-	name="file-upload"
-	accept="image/*,text/*,.html,.htm,application/*"
-	maxFileSize={1024 * 1024 * 10}
-	maxFiles={100}
-	onFileChange={console.log}
-	onFileReject={console.error}
-	allowDrop
-	classes="w-full flex flex-col items-center justify-center rounded-border"
->
-	{#snippet iconInterface()}<IconUpload class="size-8" />{/snippet}
-	{#snippet iconFile()}<IconFile class="size-7" />{/snippet}
-	{#snippet iconFileRemove()}<IconRemove class="size-7" />{/snippet}
+<FileUpload name="example-button" accept="image/*" onFileChange={console.log} maxFiles={2}>
+	<button class="btn preset-filled">
+		<IconUpload class="size-4" />
+		<span>Select File</span>
+	</button>
 </FileUpload>

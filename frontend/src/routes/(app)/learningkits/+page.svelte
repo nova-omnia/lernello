52ec52ec3d4077dda9c1452fb672a86895efc3fb
deomiarn <deomicarnel@hotmail.com>
2025-04-20 12:39:12 +0200

    feat: create all learningkits overview page

---
+++ frontend/src/routes/(app)/learningkits/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+	import DashboardBase from "$lib/components/DashboardBase.svelte";
+	import PlaceholderLearningKit from "$lib/components/learningkit/PlaceholderLearningKit.svelte";
+	import AddLearningKit from "$lib/components/learningkit/AddLearningKit.svelte";
+	import ErrorIllustration from "$lib/components/ErrorIllustration.svelte";
+	import LearningKit from "$lib/components/learningkit/LearningKit.svelte";
+	import { createQuery } from "@tanstack/svelte-query";
+	import { api } from "$lib/api/apiClient";
+	import { getAllLearningKits } from "$lib/api/collections/learningKit";
+
+	const kitsQuery = createQuery({
+		queryKey: [ 'learning-kits-list' ],
+		queryFn: () => api(fetch).req(getAllLearningKits, null).parse()
+	});
+</script>
+
+<DashboardBase title={$_('learningKit.title')}>
+	<div class="container flex space-y-4">
+		<div class="container flex h-36 flex-wrap gap-4">
+			{#if $kitsQuery.status === 'pending'}
+				{#each Array(3)}
+					<PlaceholderLearningKit />
+				{/each}
+			{:else if $kitsQuery.status === 'error'}
+				<ErrorIllustration>{$_('learningKit.error.loadList')}</ErrorIllustration>
+			{:else}
+				{#each $kitsQuery.data as kit (kit.uuid)}
+					<LearningKit title={kit.name} uuid={kit.uuid} />
+				{/each}
+				<AddLearningKit title={$_('learningKit.create')} />
+			{/if}
+		</div>
+	</div>
+</DashboardBase>

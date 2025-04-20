88d103884825b69278b94869b0a64f0f2a2be219
deomiarn <deomicarnel@hotmail.com>
2025-04-20 14:16:18 +0200

    feat: improve sidebar styling and functionality. added svelte component

--- frontend/src/lib/components/Sidebar.svelte
+++ frontend/src/lib/components/Sidebar.svelte
@@ -1,87 +1,53 @@
 <script lang="ts">
+	import { Navigation } from '@skeletonlabs/skeleton-svelte';
 	import logo from '$lib/assets/Lernello_Logo.svg';
-	import { sidebarState } from '$lib/states/sidebarState.svelte';
-	import {
-		ChartLine,
-		Folder,
-		LayoutDashboard,
-		LogOut,
-		Settings,
-		SidebarClose,
-		User
-	} from 'lucide-svelte';
 	import { _ } from 'svelte-i18n';
+	import { ChartLine, Folder, LayoutDashboard, LogOut, Settings, User } from 'lucide-svelte';
+
+	const { children } = $props();
 </script>
 
-{#snippet sidebarItemLabel(label: string)}
-	<span
-		class="ml-3 text-nowrap transition-all"
-		class:visible={sidebarState.isExpanded}
-		class:w-max={sidebarState.isExpanded}
-		class:opacity-100={sidebarState.isExpanded}
-		class:invisible={!sidebarState.isExpanded}
-		class:w-0={!sidebarState.isExpanded}
-		class:opacity-0={!sidebarState.isExpanded}>{label}</span
-	>
-{/snippet}
+<div class="card grid h-screen w-full grid-cols-[auto_1fr]">
+	<!-- Component -->
+	<Navigation.Rail expanded={true} tilesClasses="!h-12 !min-h-12 !justify-start !gap-1">
+		<!-- Header -->
+		{#snippet header()}
+			<div class="flex h-20 w-full items-center px-4">
+				<img alt="Lernello" src={logo} width="36" class="object-cover object-center" />
+				<p class="preset-typo-subtitle !text-black">{$_('app.companyName')}</p>
+			</div>
+		{/snippet}
 
-<!-- Sidebar -->
-<div
-	class="preset-filled-surface-100-900 flex h-screen flex-col items-start justify-start overflow-hidden p-4 transition-all duration-300"
-	class:w-64={sidebarState.isExpanded}
-	class:w-16={!sidebarState.isExpanded}
->
-	<div class="w-max min-w-full space-y-2">
-		<!-- Logo -->
-		<p class="flex items-center" aria-label="Lernello">
-			<img alt="Lernello" src={logo} width="24" class="object-cover object-center" />
-			{@render sidebarItemLabel('Lernello')}
-			{#if sidebarState.isExpanded}
-				<button
-					class="ml-auto items-center justify-center"
-					onclick={sidebarState.toggleSidebar}
-					aria-label="Toggle sidebar"
-					aria-expanded={sidebarState.isExpanded}
-				>
-					<SidebarClose size={24} />
-				</button>
-			{/if}
-		</p>
-
-		<hr class="hr" />
-		<!-- Buttons Section -->
-		<div class="flex flex-col items-center">
-			<a class="flex w-full items-center py-2" href="/dashboard" aria-label="Dashboard">
+		<!-- Main Tiles -->
+		{#snippet tiles()}
+			<p class="preset-typo-caption w-full px-4 pt-8">{$_('sidebar.general')}</p>
+			<Navigation.Tile labelExpanded={$_('sidebar.dashboard')} href="/dashboard">
 				<LayoutDashboard size={24} />
-				{@render sidebarItemLabel($_('sidebar.dashboard'))}
-			</a>
-			<a class="flex w-full items-center py-2" href="/folders" aria-label="Folders">
+			</Navigation.Tile>
+			<Navigation.Tile labelExpanded={$_('sidebar.folders')} href="/folders">
 				<Folder size={24} />
-				{@render sidebarItemLabel($_('sidebar.folders'))}
-			</a>
-			<a class="flex w-full items-center py-2" href="/statistics" aria-label="Statistics">
+			</Navigation.Tile>
+			<Navigation.Tile labelExpanded={$_('sidebar.statistics')} href="/statistics">
 				<ChartLine size={24} />
-				{@render sidebarItemLabel($_('sidebar.statistics'))}
-			</a>
-		</div>
+			</Navigation.Tile>
+			<p class="preset-typo-caption w-full px-4 pt-8">{$_('sidebar.configuration')}</p>
+			<Navigation.Tile labelExpanded={$_('sidebar.settings')} href="/settings">
+				<Settings size={24} />
+			</Navigation.Tile>
+			<Navigation.Tile labelExpanded={$_('sidebar.profile')} href="/profile">
+				<User size={24} />
+			</Navigation.Tile>
+		{/snippet}
 
-		<hr class="hr" />
-	</div>
-
-	<div class="mt-auto flex flex-col items-center">
-		<a class="flex w-full items-center py-2" href="/settings" aria-label="Settings">
-			<Settings size={24} />
-			{@render sidebarItemLabel($_('sidebar.settings'))}
-		</a>
-		<a class="flex w-full items-center py-2" href="/profile" aria-label="Profile">
-			<User size={24} />
-			{@render sidebarItemLabel($_('sidebar.profile'))}
-		</a>
-		<form method="POST" action="/logout" class="flex w-full items-center py-2">
-			<button class="flex" aria-label="Logout">
+		<!-- Footer -->
+		{#snippet footer()}
+			<Navigation.Tile labelExpanded={$_('sidebar.logout')} href="/logout">
 				<LogOut size={24} />
-				{@render sidebarItemLabel($_('sidebar.logout'))}
-			</button>
-		</form>
+			</Navigation.Tile>
+		{/snippet}
+	</Navigation.Rail>
+	<!-- Content -->
+	<div class="flex flex-1 flex-col">
+		{@render children()}
 	</div>
 </div>
--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -5,12 +5,9 @@
 	let { children } = $props();
 </script>
 
-<div class="flex h-full">
-	<Sidebar />
-	<div class="flex flex-1 flex-col">
-		<Topbar />
-		<main class="relative min-h-0 flex-1 overflow-auto p-4">
-			{@render children()}
-		</main>
-	</div>
-</div>
+<Sidebar>
+	<Topbar />
+	<main class="relative min-h-0 flex-1 overflow-auto p-4">
+		{@render children()}
+	</main>
+</Sidebar>

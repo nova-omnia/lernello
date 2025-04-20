4521a2ad7c3ff11f9e2d8b38aa7c18900088511a
deomiarn <deomicarnel@hotmail.com>
2025-04-20 14:35:04 +0200

    feat: improve topbar styling

--- frontend/src/lib/components/Topbar.svelte
+++ frontend/src/lib/components/Topbar.svelte
@@ -1,25 +1,14 @@
 <script lang="ts">
-	import { sidebarState } from '$lib/states/sidebarState.svelte';
-	import { SidebarOpen } from 'lucide-svelte';
-	import { page } from '$app/state';
 	import LanguageSelect from '$lib/components/LanguageSelect.svelte';
+	import { _ } from 'svelte-i18n';
 </script>
 
-<nav class="preset-filled-surface-100-900 flex min-h-13 items-center justify-between px-5 py-1">
-	{#if !sidebarState.isExpanded}
-		<button
-			class="flex h-12 w-12 items-center justify-center"
-			onclick={sidebarState.toggleSidebar}
-			aria-label="Toggle sidebar"
-		>
-			<SidebarOpen size={24} />
-		</button>
-	{/if}
-	{page.url.pathname.at(1)?.toUpperCase() + page.url.pathname.slice(2)}
-	<div
-		class="input-group disabled mr-2 ml-auto grid-cols-[auto_1fr_auto] items-center justify-center"
-	>
-		<input class="ig-input preset-tonal rounded" type="search" placeholder="Search..." />
-	</div>
+<nav class="preset-filled-surface-100-900 flex h-20 items-center justify-between px-5">
+	<input
+		class="input preset-filled-surface-50-950 h-10 w-sm"
+		disabled
+		type="search"
+		placeholder={$_('app.searchPlaceholder')}
+	/>
 	<LanguageSelect />
 </nav>

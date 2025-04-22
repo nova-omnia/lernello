6106648640d3237330e5a05f3b97607a59c1ce2b
Rajadurai Thunusan (rajadthu) <rajadthu@students.zhaw.ch>
2025-04-22 14:20:14 +0200

    feat(blocks): add multimedia upload component

---
+++ frontend/src/lib/components/blocks/BlockMultimediaItem.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { FileUpload } from '@skeletonlabs/skeleton-svelte';
+	// Icons
+
+	import { _ } from 'svelte-i18n';
+	import { FilePlus } from 'lucide-svelte';
+
+</script>
+
+<div>
+	<FileUpload
+		name="Multimedia"
+		accept="audio/*, video/*, image/*"
+		maxFiles={1}
+		onFileChange={console.log}
+		onFileReject={console.error}
+		classes="w-full"
+		label={$_('block.multimediaBlock.label')}
+
+	>
+        {#snippet iconInterface()}<FilePlus class="size-8" />{/snippet}
+	</FileUpload>
+</div>

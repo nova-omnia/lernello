86653eee5b3cbea45ce3aa1bf869bd14d36f5425
Léon Brodbeck <brodbeckleon@gmail.com>
2025-04-23 11:42:36 +0200

    feat: removed learningunitid

--- frontend/src/lib/schemas/request/UpdateBlock.ts
+++ frontend/src/lib/schemas/request/UpdateBlock.ts
@@ -5,14 +5,12 @@ export const BlockType = z.enum(['THEORY', 'MULTIPLE_CHOICE']);
 export const UpdateTheoryBlockSchema = z.object({
 	type: z.literal(BlockType.Enum.THEORY),
 	name: z.string().min(3).max(40),
-	learningUnitId: z.string().uuid().nullable().optional(),
 	content: z.string().min(1).nullable().optional()
 });
 
 export const UpdateMultipleChoiceBlockSchema = z.object({
 	type: z.literal(BlockType.Enum.MULTIPLE_CHOICE),
 	name: z.string().min(3).max(40),
-	learningUnitId: z.string().uuid().nullable().optional(),
 	question: z.string().min(1),
 	possibleAnswers: z.array(z.string().min(1)),
 	correctAnswers: z.array(z.string().min(1))

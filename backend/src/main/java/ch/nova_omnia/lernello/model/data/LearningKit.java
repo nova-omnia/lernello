34ea6c8f8f1de5ae627409fe4ddb0e3293930e49
deomiarn <deomicarnel@hotmail.com>
2025-04-25 10:28:25 +0200

    feat: created JacksonConfig for CreatedDate annotation

--- backend/src/main/java/ch/nova_omnia/lernello/model/data/LearningKit.java
+++ backend/src/main/java/ch/nova_omnia/lernello/model/data/LearningKit.java
@@ -1,22 +1,6 @@
 package ch.nova_omnia.lernello.model.data;
 
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -25,12 +9,20 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name = "learning_kits")
 @Data
 @NoArgsConstructor(force = true)
 @RequiredArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class LearningKit {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -54,10 +46,9 @@ public class LearningKit {
     @Column(name = "context")
     private String context;
 
-    @Column(name = "createdAt", nullable = false)
-    @NotNull
     @CreatedDate
-    private ZonedDateTime createdAt = ZonedDateTime.now();
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 
     @OneToMany(mappedBy = "learningKit", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<LearningUnit> learningUnits = new ArrayList<>();

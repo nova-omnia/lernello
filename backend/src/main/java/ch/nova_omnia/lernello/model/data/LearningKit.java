c01e27c0d0e1664f94d1f6cb8d54342d0892332a
Léon Brodbeck <brodbeckleon@gmail.com>
2025-04-25 14:31:48 +0200

    feat: resolve mergeconflict

--- backend/src/main/java/ch/nova_omnia/lernello/model/data/LearningKit.java
+++ backend/src/main/java/ch/nova_omnia/lernello/model/data/LearningKit.java
@@ -18,6 +18,7 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.EntityListeners;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;

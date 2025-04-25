2abfc9a00632e9aa509cd784a969cd373915dcfa
deomiarn <deomicarnel@hotmail.com>
2025-04-25 10:30:50 +0200

    feat: rename dashboardbase to pagecontainer and added optional subtitle

--- frontend/src/lib/components/PageContainer.svelte
+++ frontend/src/lib/components/PageContainer.svelte
@@ -3,13 +3,17 @@
 
 	interface BaseProps {
 		title: string;
+		subtitle?: string;
 		children: Snippet<[]>;
 	}
 
-	const { title, children }: BaseProps = $props();
+	const { title, subtitle, children }: BaseProps = $props();
 </script>
 
 <div class="space-y-4 p-4">
 	<h1 class="preset-typo-headline">{title}</h1>
+	{#if subtitle}
+		<h2 class="preset-typo-subtitle">{subtitle}</h2>
+	{/if}
 	{@render children()}
 </div>

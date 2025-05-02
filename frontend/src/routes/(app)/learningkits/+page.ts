9299be8f29d83a849a0ff2431e17f72f8aa52aa4
Léon Brodbeck <brodbeckleon@gmail.com>
2025-05-02 20:56:04 +0200

    feat: added role based view to dashboard and learningkitsview

--- frontend/src/routes/(app)/learningkits/+page.ts
+++ frontend/src/routes/(app)/learningkits/+page.ts
@@ -2,11 +2,16 @@ import { api } from '$lib/api/apiClient.js';
 import { getLearningKits } from '$lib/api/collections/learningKit';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch, parent }) => {
+export const load: PageLoad = async ({ fetch, parent, data }) => {
 	const { queryClient } = await parent();
 
 	await queryClient.prefetchQuery({
 		queryKey: ['all-learning-kits-list'],
 		queryFn: () => api(fetch).req(getLearningKits, null, { page: 0, size: 99999 }).parse()
 	});
+
+	return {
+		queryClient,
+		role: data.role
+	};
 };

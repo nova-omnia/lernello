bd63a4336995aff2e771f1ad1757f8427e283bb3
Michal Johnson <johnsmic@students.zhaw.ch>
2025-05-06 15:46:45 +0200

    refactor: simplify file item component by removing unused queries and improving file URL handling

--- frontend/src/lib/components/learningkit/displays/FileItem.svelte
+++ frontend/src/lib/components/learningkit/displays/FileItem.svelte
@@ -3,9 +3,7 @@
 	import { _ } from 'svelte-i18n';
 	import type { FileRes } from '$lib/schemas/response/FileRes';
 	import ConfirmDialog from '$lib/components/dialogs/ConfirmDialog.svelte';
-	import { createQuery } from '@tanstack/svelte-query';
-	import { getStaticFile } from '$lib/api/collections/file';
-	import { api } from '$lib/api/apiClient';
+	import { BASE_URL } from '$lib/api/apiClient';
 
 	interface FileItemProps {
 		File: FileRes;
@@ -22,39 +20,32 @@
 	}
 
 	const fileId = File.uuid;
-
-	const fileQuery = createQuery({
-		queryKey: ['file', fileId],
-		queryFn: () => api(fetch).req(getStaticFile, null, fileId).parse()
-	});
-
-	function openFileView() {
-		const fileUrl = $fileQuery.data?.fileUrl;
-		if (fileUrl) {
-			window.open(fileUrl, '_blank');
-		}
+	function getStaticFileUrl(fileId: string) {
+		return BASE_URL + '/files/' + fileId;
 	}
 </script>
 
-<div
-	class="card preset-filled-surface-100-900 flex w-full items-center justify-between p-4"
-	onclick={openFileView}
->
+<div class="card preset-filled-surface-100-900 flex w-full items-center justify-between p-4">
 	<div class="flex w-full max-w-sm items-center gap-4 truncate">
 		<FileIcon class="h-10 w-10" />
 		<p class="text-black-700 ml-3 text-xs font-bold">{File.name}</p>
 	</div>
 
-	<button
-		type="button"
-		class="btn preset-filled-error-500"
-		onclick={(e) => {
-			e.preventDefault();
-			showDeleteDialog = true;
-		}}
-	>
-		{$_('common.remove')}
-	</button>
+	<div>
+		<a href={getStaticFileUrl(File.uuid)} class="btn preset-tonal-surface">
+			{$_('common.open')}
+		</a>
+		<button
+			type="button"
+			class="btn preset-filled-error-500"
+			onclick={(e) => {
+				e.preventDefault();
+				showDeleteDialog = true;
+			}}
+		>
+			{$_('common.remove')}
+		</button>
+	</div>
 </div>
 
 <ConfirmDialog

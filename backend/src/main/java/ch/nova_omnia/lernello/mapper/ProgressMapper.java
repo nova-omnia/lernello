7656927e2e598ff0652da0fbf54208e613a31156
Léon Brodbeck <brodbeckleon@gmail.com>
2025-05-07 08:31:12 +0200

    feat: pr breakdown

---
+++ backend/src/main/java/ch/nova_omnia/lernello/mapper/ProgressMapper.java
@@ -0,0 +1,23 @@
+package ch.nova_omnia.lernello.mapper;
+
+import ch.nova_omnia.lernello.dto.response.progress.LearningKitProgressResDTO;
+import ch.nova_omnia.lernello.dto.response.progress.LearningUnitProgressDTO;
+import ch.nova_omnia.lernello.model.data.progress.LearningKitProgress;
+import ch.nova_omnia.lernello.model.data.progress.LearningUnitProgress;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ProgressMapper {
+
+    @Mapping(source = "learningKit.uuid", target = "learningKitId")
+    @Mapping(source = "opened", target = "isOpened")
+    @Mapping(source = "completed", target = "isCompleted")
+    LearningKitProgressResDTO toLearningKitProgressResDTO(LearningKitProgress learningKitProgress);
+
+    @Mapping(source = "learningUnit.uuid", target = "learningUnitId")
+    @Mapping(source = "opened", target = "isOpened")
+    @Mapping(source = "completed", target = "isCompleted")
+    LearningUnitProgressDTO toLearningUnitProgressDTO(LearningUnitProgress learningUnitProgress);
+}

bd1bb8ed1c2ae4793017259fac0b68b9fcad2188
Hajir <hajir3@gmail.com>
2025-05-08 06:52:29 +0200

    refactor: remove unused method getStoragePath from FileService interface

--- backend/src/main/java/ch/nova_omnia/lernello/service/file/FileService.java
+++ backend/src/main/java/ch/nova_omnia/lernello/service/file/FileService.java
@@ -24,7 +24,4 @@ public interface FileService {
     public String extractTextFromFiles(List<UUID> fileIds);
 
     public String extractTextFromFile(UUID fileId);
-
-    public String getStoragePath(UUID id);
-
 }

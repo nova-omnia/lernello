e3bcd5e3ffa8321eba6d7cd0ec28e05ebcfece05
Michal Johnson <johnsmic@students.zhaw.ch>
2025-05-09 18:50:51 +0200

    feat: add position field to LearningUnit and update related DTOs

--- backend/src/main/java/ch/nova_omnia/lernello/dto/request/UpdateLearningUnitOrderDTO.java
+++ backend/src/main/java/ch/nova_omnia/lernello/dto/request/UpdateLearningUnitOrderDTO.java
@@ -3,11 +3,9 @@ package ch.nova_omnia.lernello.dto.request;
 import java.util.List;
 import java.util.UUID;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateLearningUnitOrderDTO(
-                                         @NotNull @JsonDeserialize(contentAs = UUID.class) List<UUID> learningUnitUuidsInOrder
+                                         @NotNull List<UUID> learningUnitUuidsInOrder
 ) {
 }

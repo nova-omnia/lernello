170fa0f799313008baefbb3f17cbc7f9c607f527
Hajir <hajir3@gmail.com>
2025-05-11 19:19:03 +0200

    fix: remove validation for topic in AIGeneratedTheoryBlockRequest and GenerateAITheoryBlockSchema. topic now can be empty

--- backend/src/main/java/ch/nova_omnia/lernello/dto/request/block/ai/AIGeneratedTheoryBlockRequest.java
+++ backend/src/main/java/ch/nova_omnia/lernello/dto/request/block/ai/AIGeneratedTheoryBlockRequest.java
@@ -3,11 +3,10 @@ package ch.nova_omnia.lernello.dto.request.block.ai;
 import java.util.List;
 import java.util.UUID;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record AIGeneratedTheoryBlockRequest(
-                                            @NotBlank String topic,
+                                            String topic,
                                             @NotNull List<UUID> files
 ) {
 }

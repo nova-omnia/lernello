09218a93d85cd659719401aa4ddb045b6ea0c2ad
Léon Brodbeck <brodbeckleon@gmail.com>
2025-05-12 16:43:01 +0200

    feat: implemented block of contents

--- frontend/src/lib/components/blocks/BlockReorder.svelte
+++ frontend/src/lib/components/blocks/BlockReorder.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import { type DndEvent, dragHandleZone, TRIGGERS } from 'svelte-dnd-action';
 	import { flip } from 'svelte/animate';
-	import BlockReorderItem from '$lib/components/blocks/BlockReorderItem.svelte';
+	import BlockReorderItem from '$lib/components/blocks/BlockOverviewItem.svelte';
 	import { blockActionState, queueBlockAction } from '$lib/states/blockActionState.svelte';
 	import type { BlockRes } from '$lib/schemas/response/BlockRes';
+	import { INSTRUCTOR_ROLE } from '$lib/schemas/response/UserInfo';
 
 	let blocksSnapshot = $derived(
 		blockActionState.blocks.map((block) => ({ ...block, id: block.uuid }))
@@ -55,7 +56,7 @@
 	>
 		{#each blocksSnapshot as block (block.id)}
 			<div class="block" animate:flip={{ duration: 200 }}>
-				<BlockReorderItem {block} />
+				<BlockReorderItem {block} role={INSTRUCTOR_ROLE} />
 			</div>
 		{/each}
 	</div>
